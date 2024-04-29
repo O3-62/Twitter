34d4ec3c339ce92713c92f711fb64abc15153787
O3 <yoonsol0602@gmail.com>
2024-04-29 11:24:45 +0900

    Twitter 서버 초기 세팅2

--- router/tweets.js
+++ router/tweets.js
@@ -1,47 +1,15 @@
 import express from "express";
+import * as tweetController from '../Controller/tweet.js';
 
 const router = express.Router();
 
-let tweets = [
-    {
-        id:'1',
-        text: '안녕하세요',
-        createdAt : Date.now().toString(),
-        name: '김사과',
-        username: 'apple',
-        url: 'https://www.nongsaro.go.kr/cms_contents/976/20151215141317605.gif'
-    },
-    {
-        id:'3',
-        text: '집에가서자고싶다...',
-        createdAt : Date.now().toString(),
-        name: '바나나',
-        username: 'banana',
-        url: 'https://img.freepik.com/premium-vector/banana-cute-kawaii-style-fruit-character-vector-illustration_787461-1772.jpg'
-    },
-]
+//트윗 찾기, 유저이름
+router.get('/',tweetConttroller.getTweets);
 
-router.get('/',(req,res) => {
-    const username = req.query.username;
-    const data = username ? tweets.filter((tweet) => tweet.username == username) : tweets;
-    
-    res
-    .status(200)
-    .json(data);
+ //트윗 찾기
+router.get('/:id',tweetConttroller.getTweets);
 
-
- });
-
-router.get('/:id',(req,res,next) =>{
-    const id = req.params.id;
-    const tweet = tweets.find((tweet) => tweet.id === id);
-    if(tweet){
-        res.status(200).json(tweet);
-    }else{
-        res.status(404).json({message: `${id}의 작성 트윗이 없습니다.`})
-    };
-});
-
+//트윗하기
 router.post('/',(req,res,next) => {
     const {text, name, username} = req.body;
     const tweet = {
@@ -66,7 +34,6 @@ router.put('/:id',(req,res,next) => {
         res.status(404).json({message: `${id}의 작성 트윗이 없습니다.`})
     };
 });
-
 router.delete('/:id',(req,res,next) => {
     const id = req.params.id;
     tweets = tweets.filter((tweet) => tweet.id !== id);
